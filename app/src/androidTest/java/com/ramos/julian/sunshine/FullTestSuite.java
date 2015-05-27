59ba6a4451ab39b012ddfa34fd3f11ea8002b7d7
Julian Ramos <ing.julianr@gmail.com>
2015-05-27 16:44:55 -0700

    Added all of the tests from sections 4 A and B. However have not tested or run the app on the phone for some time since all the code added does not change visibly the app.

---
+++ app/src/androidTest/java/com/ramos/julian/sunshine/FullTestSuite.java
@@ -0,0 +1,22 @@
+package com.ramos.julian.sunshine;
+
+/**
+ * Created by julian on 5/22/15.
+ */
+
+
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class FullTestSuite extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
+    }
+
+    public FullTestSuite() {
+        super();
+    }
+}
